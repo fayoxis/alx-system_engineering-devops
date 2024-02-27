0af80506be27b31014575f617313df14f74016b5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-27 18:08:48 +0100

    Update 4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match the given cases
-
+# The script accepts an argument then passes it to a regular expression
 puts ARGV[0].scan(/hbt*n/).join
