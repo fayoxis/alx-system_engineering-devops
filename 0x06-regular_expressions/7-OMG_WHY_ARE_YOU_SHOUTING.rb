14d4300e002885d1f2e288b62d20437a4f5c9210
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-27 18:10:51 +0100

    Update 7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# This script should output: [SENDER],[RECEIVER],[FLAGS]
-
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+# regular expression that matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
