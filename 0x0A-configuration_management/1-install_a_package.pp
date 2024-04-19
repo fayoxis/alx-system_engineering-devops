800924ffd7600ff373b2f0942db0243c0a9bc4e8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-19 23:16:37 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
+#  Puppet, install flask from pip3
+
+package = {
+  name     => 'flask',
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+package { $package['name']:
+  ensure   => $package['ensure'],
+  provider => $package['provider'],
 }
