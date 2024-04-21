11835bda0a0b90e3f715a147c7576eb1d8a7ffc7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-21 20:09:40 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # Manage SSH config file
 file_line {
-  default:
-    path => '/etc/ssh/ssh_config', ; Declare common path
+  default: 
+    path => '/etc/ssh/ssh_config',
+
   'Turn off passwd auth':
     ensure => 'present',
     line   => '    PasswordAuthentication no',
-  'Declare identity file':      
+
+  'Declare identity file':
     ensure => 'present',    
-    line   => '    IdentityFile ~/.ssh/school',
+    line   => '    IdentityFile ~/.ssh/school', 
 }
