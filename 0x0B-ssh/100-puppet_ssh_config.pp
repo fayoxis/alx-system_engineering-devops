283411d71ef766ee8c14d4e975a8681db0433f42
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-21 18:47:20 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,22 @@
-#  Config SSH config file for automated passwordless connection using Puppet
-
 include stdlib
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+
+# Manage SSH config file
+$ssh_config_file = '/etc/ssh/ssh_config'
+
+# Disable password authentication
+$password_authentication_line = '    PasswordAuthentication no'
+augeas { 'disable_password_auth':
+  context => "/files${ssh_config_file}",
+  changes => [
+    "set Host[.='']/PasswordAuthentication ${password_authentication_line}"
+  ],
 }
 
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true,
+# Set the identity file for SSH
+$identity_file_line = '    IdentityFile ~/.ssh/school'
+augeas { 'set_identity_file':
+  context => "/files${ssh_config_file}",
+  changes => [
+    "set Host[.='']/IdentityFile ${identity_file_line}"
+  ],
 }
