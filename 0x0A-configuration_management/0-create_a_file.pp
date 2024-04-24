a93d14806b2f7a780ef28c9412f6a7be81016b67
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-25 00:02:26 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,17 +1,8 @@
 # This is a  manifest creates  at /tmp
-$tmp_dir = '/tmp'
-$file_name = 'school'
-$file_path = "${tmp_dir}/${file_name}"
-$file_mode = '0744'
-$file_owner = 'www-data'
-$file_group = 'www-data'
-$file_content = 'I love Puppet'
-
-file { $file_path:
-  ensure  => present,
-  path    => $file_path,
-  mode    => $file_mode,
-  owner   => $file_owner,
-  group   => $file_group,
-  content => $file_content,
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
