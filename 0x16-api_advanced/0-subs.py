15a228b0a9ad7d66317c4dcb696d43e5bb88325b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-04 12:17:26 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-
-
 def number_of_subscribers(subreddit):
+
+    
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
