084aefa03d7e9a80c3a776df17ab57d2e1c2a229
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-04 12:41:13 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns a list containing the
-titles of all hot articles for a given subreddit."""
+""" the Module that consumes the Reddit API and returns a list
+articles."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], n=0, after=None):
-    """ queries the Reddit API and returns a list containing the titles of
-    all hot articles for a given subreddit
-
-    The Reddit API uses pagination for separating pages of responses.
-    If not a valid subreddit, return None.
-
-    Args:
-        subreddit (str): subreddit.
-        hot_list (list, optional): list of titles. Defaults to [].
-
-    Returns:
-        list: list of titles.
+    """ this queries the API and returns a list containing the titles of
+    all articles
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'custom'}
     r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 200:
+    while r.status_code == 200:
         r = r.json()
         for post in r.get('data').get('children'):
             hot_list.append(post.get('data').get('title'))
-        if r.get('data').get('after'):
+        while r.get('data').get('after'):
             recurse(subreddit, hot_list)
         return hot_list
     else:
