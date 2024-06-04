aeddd4fc816fd42db602a0bee3d96e3832f84e4b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-04 12:16:46 +0100

    Create 0-main.py

---
+++ 0x16-api_advanced/0-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+import sys
+
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+wintermancer@lapbox ~/reddit_api/project $ python3 0-main.py programming
+756024
+wintermancer@lapbox ~/reddit_api/project $ python3 0-main.py this_is_a_fake_subreddit
+0
