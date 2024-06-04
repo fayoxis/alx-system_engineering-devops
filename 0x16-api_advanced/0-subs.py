ce17e61a5230ffce92da15a6019b7c2bf23c43f4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-04 12:26:14 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -11,6 +10,8 @@ def number_of_subscribers(subreddit):
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
-        return (0)
-    results = response.json().get("data")
-    return (results.get("subscribers"))
+        return 0
+    data = response.json().get("data")
+    if data:
+        return data.get("subscribers", 0)
+    return 0
