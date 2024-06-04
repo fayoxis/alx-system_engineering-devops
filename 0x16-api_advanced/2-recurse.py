48dec0abfe2ccb1e0cab69a459c0f2fe554100d9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-04 12:46:46 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" the Module that consumes the Reddit API and returns a list
-articles."""
+"""
+this will Use reddit's API
+"""
+
+
 import requests
+after = None
 
 
-def recurse(subreddit, hot_list=[], n=0, after=None):
-    """ this queries the API and returns a list containing the titles of
-    all articles
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'custom'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    while r.status_code == 200:
-        r = r.json()
-        for post in r.get('data').get('children'):
-            hot_list.append(post.get('data').get('title'))
-        while r.get('data').get('after'):
+def recurse(subreddit, hot_list=[]):
+    """will be returning top ten post titles"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    while results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        while after_data is not None:
+            after = after_data
             recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
-        return None
+        return (None)
