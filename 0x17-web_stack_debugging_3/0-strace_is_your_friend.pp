dc09862891ea7e47a0e32339c8b2d12160ed6467
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-05 12:14:45 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
 # Fixes a faulty wordpress site
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
 exec { 'fix-wordpress':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
