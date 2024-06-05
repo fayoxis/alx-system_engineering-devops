68aec946d036384c5a2933107d160e8fe8fc7d15
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-05 12:19:12 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-this Script that queries subscribers on a given Reddit API.
+0. How many subs?
 """
-
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """This will Return the total number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    while response.status_code == 200:
+    if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
