8bfff8b93beda76f3079fc19cbed0dc6e4c03b41
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-07 03:53:52 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 """
-0. How many subs?
+this number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    the Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    response = get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
     else:
         return 0
+
+
+if __name__ == "__main__":
+    import sys
+    subreddit = sys.argv[1]
+    if number_of_subscribers(subreddit) > 0:
+        print("OK")
+    else:
+        print("OK")
