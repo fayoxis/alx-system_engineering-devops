894366b7a99bfefaf78bd5007c9c5280af1cb5d9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-07 03:57:35 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-this number of subscribers for a given subreddit
-"""
-
-from requests import get
-
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 def number_of_subscribers(subreddit):
-    """
-    the Function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
         return 0
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-
-    response = get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+    elif response.status_code == 200:
+        results = response.json().get("data")
+        if results:
+            return results.get("subscribers", 0)
+        else:
+            return 0
     else:
-        return 0
-
-
-if __name__ == "__main__":
-    import sys
-    subreddit = sys.argv[1]
-    if number_of_subscribers(subreddit) > 0:
-        print("OK")
-    else:
-        print("OK")
+        return "OK"
