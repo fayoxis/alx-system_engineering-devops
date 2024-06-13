aa57627c645e2ae7cae22f3ea0725bfe5fdee819
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-13 15:58:30 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
-# This manuscript enables the user holberton to login and open files without error
+# enables the user holberton to login and open files without error
 
-# Increase hard file limit for user holberton
+# Increase hard file limit
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for user holberton
+# Increase soft file
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
